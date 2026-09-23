e49ed38c1cec9b1718f226dc80c1d9b60c04f614
Harish <harish191012@gmail.com>
2026-09-24 02:24:46 +0530

    fix CI headless configuration

--- playwright.config.js
+++ playwright.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
 
     baseURL: 'https://www.saucedemo.com',
 
-    headless: !process.env.CI,
+    headless: !!process.env.CI,
 
     screenshot: 'only-on-failure',
 
