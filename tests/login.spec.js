5ebdd027a013887a0ef949335d16ec750ff67193
Harish <harish191012@gmail.com>
2026-09-24 00:47:43 +0530

    added day2 login execution log

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -35,7 +35,8 @@ test("Product sorting", async({page})=>
      await loginPage.login('standard_user', 'secret_sauce');
      await sorting.productclick();
      await sorting.sorting_Verification();
-     console.log("feature branch")
+     console.log("feature branch");
+     console.log("day2 commits");
      
 
 
