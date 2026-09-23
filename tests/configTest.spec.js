1fa64296f7a399576ec51c2059e8b40023f2c1c5
Harish <harish191012@gmail.com>
2026-09-24 02:38:25 +0530

    fix config test title assertion

--- tests/configTest.spec.js
+++ tests/configTest.spec.js
@@ -5,7 +5,7 @@ test('Verify framework configuration', async ({ page }) => {
     await page.goto('/');
 
     // await expect(page).toHaveTitle(/Swag Labs/);
-    await expect(page).toHaveTitle(/WrongTitle/);
+    await expect(page).toHaveTitle(/Swag Labs/);
 
     console.log('URL:', page.url());
 
